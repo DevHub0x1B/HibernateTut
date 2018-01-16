8e29881669aa17c83d0f941bee3f73bd71ec62a7
Mohamed.Ismail <Mohamed.ismail@ssi-schaefer.com>
2018-01-16 17:39:39 -0500

    Hibernate Tutorial - Full Directory

--- src/com/company/hibernate/tryout/Main.java
+++ src/com/company/hibernate/tryout/Main.java
@@ -1,8 +1,32 @@
 package com.company.hibernate.tryout;
 
+import com.company.hibernate.tryout.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            Student tempStudent = new Student("moe", "money", "moe@gmail.com");
+            session.beginTransaction();
+            session.save(tempStudent);
+            session.getTransaction().commit();
+
+            System.out.println("Student registered!");
+
+
+        } finally { //no catch ?!
+            factory.close();
+        }
+
+
     }
 }
